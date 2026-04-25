1e841b92347da00d4c2235d497d8274556d49aca
Andrii Khomenko <akhomenko1995@gmail.com>
2026-04-25 17:34:44 +0200

    api

--- app/api/api.ts
+++ app/api/api.ts
@@ -2,7 +2,9 @@ import axios, { AxiosError } from 'axios';
 
 export type ApiError = AxiosError<{ error: string }>;
 
+const baseURL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
+
 export const api = axios.create({
-  baseURL: 'https://notehub-api.goit.study',
+  baseURL,
   withCredentials: true,
 });
